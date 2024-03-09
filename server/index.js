ad239d5489bbc72067efbdf13217149f04d1349d
Min Park <Min@Mins-MacBook-Air.local>
2024-03-08 23:44:39 -0800

    Get more data from Google Book API

--- server/index.js
+++ server/index.js
@@ -15,35 +15,62 @@ let bookNameList = [
   "demian",
   "harry potter halfblood",
 ];
-// will be replaced with list of book names in postgres
 
-let bookURLList;
-async function returnsURLList() {
-  bookURLList = [];
+let bookInfo;
+// let bookInfo = [
+//   {
+//     title: "animal farm",
+//     authors: [ 'George Orwell' ],
+//     image: "http://books.google.com/books/content?id=yDQJ6y2LSX8C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+//   },
+//   {
+//     title: "Harry Potter and the Half-Blood Prince",
+//     authors: [ 'J.K. Rowling' ],
+//     image: "http://books.google.com/books/content?id=R7YsowJI9-IC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+//   },
+//   {
+//     title: "Demian",
+//     authors: [ 'Hermann Hesse' ],
+//     image: "http://books.google.com/books/content?id=Uf4GdPn5EiEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+//   },
+//   {
+//     title: "Harry Potter and the Chamber of Secrets",
+//     authors: [ 'J.K. Rowling' ],
+//     image: "http://books.google.com/books/content?id=5iTebBW-w7QC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+//   },
+// ]
+// will be replaced with postgres
+
+async function returnsBookInfo() {
+  bookInfo = [];
   const promises = bookNameList.map(async (name, index) => {
     const response = await axios.get(
       `https://www.googleapis.com/books/v1/volumes?q=${name}`
     );
-    const result = response.data.items[0].volumeInfo.imageLinks.thumbnail;
-    bookURLList[index] = result;
+    const result = response.data.items[0].volumeInfo;
 
-    return result;
+    const {imageLinks, title, authors, description} = result; //title: string // authors: array
+    const image = imageLinks.thumbnail;
+    bookInfo.push({title, authors, image, description})
+    // bookInfo[index] = result;
+
+    return {title, authors, image, description};
   });
 
-  const updatedURLList = Promise.all(promises).then((result) => {
+  // to make the function wait for .map to execute completely before returning
+  const updatedBookInfo = Promise.all(promises).then((result) => {
     return result;
   });
-
-  return updatedURLList;
+  return updatedBookInfo;
 }
 
 async function filtersURLList(index) {
-  bookURLList.splice(index, 1);
-  return bookURLList;
+  bookInfo.splice(index, 1);
+  return bookInfo;
 }
 
 app.get("/api", async (req, res) => {
-  const result = await returnsURLList();
+  const result = await returnsBookInfo();
   res.send({ message: result });
 });
 
@@ -53,16 +80,13 @@ app.post("/api", async (req, res) => {
   // adds data to the list if data exists (not undefined)
   data ? bookNameList.push(data) : null; //******** should be changed to SQL code INSERT INTO
 
-  const result = await returnsURLList();
-  console.log(bookURLList);
+  const result = await returnsBookInfo();
   res.send({ message: result });
 });
 
 app.post("/api/delete", async (req, res) => {
   const index = req.body.index;
-
   const result = await filtersURLList(index);
-  console.log(bookURLList);
   res.send({ message: result });
 });
 
