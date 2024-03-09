8b69887e5820fe87c92ba942181589bbf5817e29
Min Park <Min@Mins-MacBook-Air.local>
2024-03-08 23:46:22 -0800

    Add title, authors, description, and Remove button

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Book from "./book";
 function App() {
   const [data, setData] = React.useState();
   const [bookName, setBookName] = React.useState();
+  const [hideButton, setHideButton] = React.useState(true);
+  //const [bookIndex, setBookIndex] = React.useState();
 
   function handleChange(event){
     setBookName(event.target.value)
@@ -32,22 +34,36 @@ function App() {
     })
   }
 
+  function mouseOn(index){
+    setHideButton(false);
+    //setBookIndex(index)
+  }
 
+  function mouseOff(index){
+    setHideButton(true);
+    //setBookIndex(index)
+  }
 
   return (
     <div className="App">
       <div className="beige-nav">
         <h1>Book List</h1>
       </div>
-      <button onClick={addBook}>Add Book</button>
-      <input placeholder="enter book title" onChange={handleChange}/>
+      <div style={{padding: "20px"}}>
+        <button onClick={addBook}>Add Book</button>
+        <input placeholder="enter book title" onChange={handleChange}/>
+      </div>
       <div className="book-boxes">
-        {data?.map((url, index) => (
+        {data?.map((bookInfo, index) => (
           <Book 
           key={index} 
           index={index} 
-          imageURL={url} 
+          bookInfo={bookInfo} // JS object with {title, authors, image}
           deleteBook={deleteBook}
+          mouseOn={mouseOn}
+          mouseOff={mouseOff}
+          hideButton={hideButton}
+          //bookIndex={bookIndex}
           />
         ))}
       </div>
--- client/src/book.js
+++ client/src/book.js
@@ -1,8 +1,49 @@
-export default function Book(props){
-    return (
-        <div className="book-cover">
-        <img style={{paddingBottom: "5px"}} src={props.imageURL} alt="book cover"/>
-        <br/><button onClick={() => props.deleteBook(props.index)}>Remove</button>
+import React from "react";
+
+export default function Book(props) {
+  const { title, authors, image, description } = props.bookInfo;
+  const [showDesc, setShowDesc] = React.useState(false);
+  const [showButton, setShowButton] = React.useState(false);
+
+  function toggleDescription() {
+    setShowDesc(!showDesc);
+  }
+
+  function mouseOn() {
+    setShowButton(true);
+  }
+
+  function mouseOff() {
+    setShowButton(false);
+  }
+
+  return (
+    <div
+      className="book-cover"
+      onClick={toggleDescription}
+      onMouseEnter={mouseOn}
+      onMouseLeave={mouseOff}
+    >
+      <p className="description" style={{ visibility: showDesc || "hidden" }}>
+        {!description ? "No description" : description} 
+      </p>
+      <div style={{ opacity: showDesc ? "0.1" : "1" }}>
+        <div style={{ paddingBottom: "10px" }}>
+          <button
+            style={{ visibility: (!showButton || showDesc) ? "hidden" : null }}
+            onClick={() => props.deleteBook(props.index)}
+          >
+            Remove
+          </button>
         </div>
-    )
+        <div>
+          <img src={image} alt="book cover" style={{border: "solid #aaa 0.1px"}} />
+        </div>
+        <div className="bookInfo-text">
+          <p style={{ fontWeight: "bold" }}>{title}</p>
+          <p>{authors.join(", ")}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
